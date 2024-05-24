971321281008d87b0d0a152dcb440e92c7d6fb8c
vararaghu002 <121346843+vararaghu002@users.noreply.github.com>
2024-05-24 07:08:31 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value(denote empty)like for temprature=0 not used(for 0 it has some value); there null is used to 
+//indicate empty 
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
