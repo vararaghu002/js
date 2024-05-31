81e5c699100a59957f3b35da2d4ef0987a706342
vararaghu002 <121346843+vararaghu002@users.noreply.github.com>
2024-05-31 20:36:36 +0530

    Update 05_String.js

--- 01_basics/05_String.js
+++ 01_basics/05_String.js
@@ -22,7 +22,9 @@ console.log(newString);
 
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
-
+//substring() treats negative indices as 0, effectively ignoring them, while slice() allows negative indices to count from the end of the string.
+//Both methods extract characters up to, but not including, the end index. If the start index is greater than the end index,
+// substring() will swap them before extraction, while slice() will return an empty string.
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
@@ -31,6 +33,6 @@ const url = "https://hitesh.com/hitesh%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+console.log(url.includes('sundar'))//to check whether string present or not
 
 console.log(gameName.split('-'));
